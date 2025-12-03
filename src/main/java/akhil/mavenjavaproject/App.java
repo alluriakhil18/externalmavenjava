443d4ae2f601d82008574a1a29caa0a9f0f33351
alluriakhil18 <alluriakhil18@gmail.com>
2025-12-03 10:30:18 +0530

    initial commit

---
+++ src/main/java/akhil/mavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package akhil.mavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the code for the external exam" );
+    }
+}
